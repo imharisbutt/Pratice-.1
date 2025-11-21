e61ed4d589efb1d8ed41c0d36232e3334418facd
Haris devsloop <harisdevsloop@gmail.com>
2025-11-21 01:53:11 -0800

    Add files via upload

--- Pratice-1.py
+++ Pratice-1.py
@@ -1,1 +1,9 @@
-
+# task 2 
+school={"Name":"Ali","Age":19,"Subject":"computer science"}
+print(school)
+school["class"]= 10
+print(school)
+school["Age"]= 20
+print(school)
+for key, value in school.items():
+     print(f"{key}:{value}")
